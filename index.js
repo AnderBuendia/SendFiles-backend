87180d72f7feb8be0f32d48b78e0d23581403736
Ander Buendia <ibysytho@gmail.com>
2020-11-17 14:39:36 +0100

    upload folder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./db/db');
 const cors = require('cors');
+const path = require('path');
+const { existsSync, mkdirSync } = require('fs');
 require('dotenv').config({path: 'config/variables.env'});
 
 /* Create server */
@@ -31,6 +33,11 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/links', require('./routes/links'));
 app.use('/api/files', require('./routes/files'));
+
+/* Uploads dir */
+existsSync(path.join(__dirname, 'uploads')) || mkdirSync(path.join(__dirname, "uploads"));
+app.use("uploads", express.static(path.join(__dirname, "uploads")));
+
 
 /* App Setup */
 app.listen(port, '0.0.0.0', () => {
